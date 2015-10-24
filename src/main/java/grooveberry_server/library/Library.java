b66455b1d881cc3ef29804506ba9d942ee95a746
Seikomi <nicolas.symphorien@gmail.com>
2015-10-24 18:39:29 +0200

    And Library files are here !

---
+++ src/main/java/grooveberry_server/library/Library.java
@@ -0,0 +1,37 @@
+package grooveberry_server.library;
+
+import grooveberry_server.audiofile.AudioFile;
+
+import java.io.File;
+import java.util.List;
+
+public class Library {
+	private List<AudioFile> audioFileList;
+	//private File libraryFile;
+	
+	public Library() {
+		this(null);
+	}
+	
+	public Library(List<AudioFile> audioFileList) {
+		this.audioFileList = audioFileList;
+	}
+	
+	public boolean isEmpty() {
+		return audioFileList.isEmpty();
+	}
+	
+	public void add(AudioFile audioFile) {
+		audioFileList.add(audioFile);
+		//libraryFile.add();
+	}
+	
+	public void remove(AudioFile audioFile) {
+		audioFileList.remove(audioFile);
+	}
+	
+	public void clear() {
+		audioFileList.clear();
+	}
+
+}
