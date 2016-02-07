13ea1d0f4c525104e0ceb390195a51922fe75194
Seikomi <nicolas.symphorien@gmail.com>
2016-02-07 23:26:16 +0100

    Translate Javadoc

--- src/main/java/grooveberry_server/server/net/Server.java
+++ src/main/java/grooveberry_server/server/net/Server.java
@@ -16,25 +16,19 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 /**
- * La classe Server construit un server de musique pilotable à distance par sockets
- * Java. <br/>
- * Il utilise les deux ports définies par les parametres de son constructeur:
- * <ul>
- * <li>{@code int serverCommandePort} : port gérant les commandes
- * (TODO définie selon un protocole) envoyées au serveur</li>
- * <li>{@code int serverTransfertPort} : port gérant les données
- * (fichiers audios, xml, ...) envoyées au serveur</li>
- * </ul>
  * <p>
- * Son but est de fournir les services suivants :
- * <ul>
- * <li>gestion de la piste en cours de lecture : #PLAY, #PAUSE</li>
- * <li>gestion de la file de lecture : #PREV, #NEXT</li>
- * <li>gestion du volume : #VOLUP, #VOLDOWN</li>
- * <li>recuperation d'information sur le serveur : #SONG, #LIST</li>
- * <li>gestion du transfert de fichiers : #UPLOAD@[nom du fichier]...,
- * #DOWNLOAD@[nom du fichier]...</li>
- * </ul>
+ * 	 Server class to build an instance of a <i>Grooveberry Server</i>. <br />
+ * 	 <i>Grooveberry Server</i> is a remote sever to control a music playlist.
+ * </p>
+ * <p>
+ * 	 They provide this sevices:
+ * 	 <ul>
+ * 	   <li>audio track management (play, pause, stop) ;</li>
+ * 	   <li>reading queue management (start, stop, next, prev, randomize) ;</li>
+ * 	   <li>volume management (volume up, volume down, mute) ;</li>
+ * 	   <li>provide state information (get the current track play, get the reading queue ).</li>
+ *   </ul>
+ * </p>
  * 
  * @author  Nicolas Symphorien
  * @since   1.0
@@ -42,38 +36,44 @@ import org.slf4j.LoggerFactory;
 public class Server {
     private final static Logger LOGGER = LoggerFactory.getLogger(Server.class);
 
-    public static final String userHomePath = System.getProperty("user.home");
+    public static final String USER_HOME_PATH = System.getProperty("user.home");
 	
-    private ServerSocket serverSocketCommande;
-    private ServerSocket serverSocketTransfert;
+    private ServerSocket commandServerSocket;
+    private ServerSocket dataServerSocket;
 
     private Thread connectionClientsThread;
     
 	/**
-	 * Créer une instance de serveur GrooveBerry utilisant deux sockets.
+	 * Construct an {@code Server} instance listen on the two ports :
+	 * <ul>
+	 * 	 <li>{@code serverCommandPort} : the command port to send command to the server ;</li>
+	 * 	 <li>{@code serverDataPort} : the data port to receive and send data to the server.</li>
+	 * </ul>
 	 * 
-	 * @param serverCommandePort
-	 * @param serverTransfertPort
+	 * @param serverCommandPort the command port of the {@code Server} instance 
+	 * @param serverDataPort the data port of the {@code Server} instance 
 	 * @throws InterruptedException
 	 */
-    public Server(int serverCommandePort, int serverTransfertPort) throws InterruptedException {
+    public Server(final int serverCommandPort, final int serverDataPort) throws InterruptedException {
     	try {
-            this.serverSocketCommande = new ServerSocket(serverCommandePort);
-            this.serverSocketTransfert = new ServerSocket(serverTransfertPort);
+            this.commandServerSocket = new ServerSocket(serverCommandPort);
+            this.dataServerSocket = new ServerSocket(serverDataPort);
         } catch (IOException e) {
             LOGGER.error("Unexpexted socket deconnection", e);
         }
     }
     
     /**
-     * Démarre le serveur GrooveBerry, créer les dossiers et fichiers
-     * nécessaires et initialise la file de lecture avec les fichiers
-     * dans ~/.grooveberry/library/ (UNIX seulement) 
+     * Start the Groovery server and initialize the reading queue with all the file in the 
+     * library. <br />
+     * If this is the first lauch on this machine, make the libray directory in 
+     * {@code HOME/.groovebery/library/} and add the .properties file in 
+     * {@code HOME/.groovebery/grooveberry.properties}.
      */
     public void start() {
     	initServerFiles();
         initReadingQueue();
-        startThreadsServer();
+        startClientAcceptThread();
     }
     
     public void restart() {
@@ -85,12 +85,12 @@ public class Server {
     }
         
     /**
-     * Initialise les fichiers et dossiers nécessaires au serveur.
+     * Initialize library directory and the .properties file.
      */
     private void initServerFiles() {
-    	Path mainDirectoryPath = Paths.get(userHomePath	+ "/.grooveberry/");
-    	Path serverPropertiesPath = Paths.get(userHomePath + "/.grooveberry/grooveberry.properties");
-    	Path serverLibraryDirectoryPath = Paths.get(userHomePath + "/.grooveberry/library/");
+    	Path mainDirectoryPath = Paths.get(USER_HOME_PATH	+ "/.grooveberry/");
+    	Path serverPropertiesPath = Paths.get(USER_HOME_PATH + "/.grooveberry/grooveberry.properties");
+    	Path serverLibraryDirectoryPath = Paths.get(USER_HOME_PATH + "/.grooveberry/library/");
     	
     	File mainDirectory = mainDirectoryPath.toFile();
     	File serverProperties = serverPropertiesPath.toFile();
@@ -128,17 +128,14 @@ public class Server {
     }
     
     /**
-     * Rempli la file de lecture avec le contenue du dossier library/
+     * initialize the reading queue with all the file in the 
+     * library.
      */
     private void initReadingQueue() {
         try {
-            Path directoryPath = Paths.get(userHomePath + "/.grooveberry/library/");
+            Path directoryPath = Paths.get(USER_HOME_PATH + "/.grooveberry/library/");
             
-            if (directoryPath.toFile().exists())
-            	
-            	// LibraryManager.getInstance().scanDirectory();
-            	
-
+            if (directoryPath.toFile().exists()) {
 	            LOGGER.debug("Scanning audio files in directory : " + directoryPath.toAbsolutePath());
 	            AudioFileDirectoryScanner directoryScanner = new AudioFileDirectoryScanner(directoryPath);
 	
@@ -147,8 +144,9 @@ public class Server {
 	            if (audioFileList.size() != 0) {
 	            	ReadingQueueManager.getInstance().addToReadingQueue(audioFileList);
 	            } else {
-	            	LOGGER.debug("No audio files in " + userHomePath + "/library/ (normal if this is the first launch)");
+	            	LOGGER.debug("No audio files in " + USER_HOME_PATH + "/library/ (normal if this is the first launch)");
 	            }
+            }
         } catch (IOException e) {
             LOGGER.error("Directory scanning error", e);
         }
@@ -156,10 +154,10 @@ public class Server {
     }
     
     /**
-     * Lance le thread de gestions des clients
+     * Start the client accept thread
      */
-    private void startThreadsServer() {
-        connectionClientsThread = new Thread(new ClientAccept(serverSocketCommande, serverSocketTransfert));
+    private void startClientAcceptThread() {
+        connectionClientsThread = new Thread(new ClientAccept(commandServerSocket, dataServerSocket));
         connectionClientsThread.setName("ClientAccept");
         connectionClientsThread.start();
         LOGGER.debug("Start ClientAccept Thread");
